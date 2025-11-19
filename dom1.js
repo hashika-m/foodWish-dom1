86b583ef4f34a4250777af66b9d8e72e92ae83c0
hashika-m <hashika0302@gmail.com>
2025-11-19 22:30:19 +0530

    Add files via upload

--- dom1.js
+++ dom1.js
@@ -1,4 +1,37 @@
+// console.log(document.body.children[0].innerText='Hello to dom1 chnge by dom' )
 
+//creating object using DOM-js  
+//  const student={
+//     name:'tiya',
+//     place:'chennai',
+
+//   greetUser(){
+//         return `Hi My name is ${this.name} and coming from ${this.place}. This msg is created using js object and function`
+//     }
+ 
+//  }
+//     // student.name='Taniya'
+//     // student.place='coonor'
+
+//     // student.name='Riya'
+//     // student.place='ooty'
+//     const userName=prompt('Enter your name')
+//    const userPlace= prompt('enter your place')
+//   //assiging the username,userplace(value) to name and place varibles
+//    student.name=userName
+//    student.place=userPlace
+
+
+//  const greet=student.greetUser()
+//  console.log(greet) //-->console.log() method
+// document.write(greet) //--> display the return statement in document viewport  ---> write() method
+// document.body.innerText=greet //-->innertext
+// document.body.innerText='Hi this msg is crated using innertext'
+ //-->innertext: this replace the student block node to the text present inside the inntertext
+// document.body.innerHTML='<h1>THsi is created using innerHTML</h1>'
+//-->this completely replace the previous set of block to new block with its content
+
+// console.log(document.documentElement.lang)
 
 
 // js for foodwish
